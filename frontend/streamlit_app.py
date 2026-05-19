4ea750af48863ad356f2306da802f612287d950f
Arush <arushrai007@gmail.com>
2026-05-19 11:50:25 +0530

    update API URL to render backend

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -9,7 +9,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-API_URL = "http://127.0.0.1:8000" 
+API_URL = "https://doc-intelligence-r2kv.onrender.com"
 
 # ---- 2. STATE MANAGEMENT ----
 if "messages" not in st.session_state:
